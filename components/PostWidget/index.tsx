71e3da1ec201081d308d48c60def72e3efa319a1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-04-01 18:02:41 +0900

    update

--- components/PostWidget/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react'
-import moment from 'moment'
-import Link from 'next/link'
-
-const PostWidget = () => {
-  return <div>PostWidget</div>
-}
-
-export default PostWidget
